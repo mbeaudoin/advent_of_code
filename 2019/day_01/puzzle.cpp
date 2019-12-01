8c42a7831e362f6ffb2f6c5f317df7e7f4784f76
Martin Beaudoin <beaudoin.martin@gmail.com>
2019-12-01 11:15:39 -0500

    Adjusting day #1 solution to new C++ basic template starting code Add robustness for comouting fuel (cannot be negative) Refactor puzzle #2 code using a do..while loop. Cleaner.

--- 2019/day_01/puzzle.cpp
+++ 2019/day_01/puzzle.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cassert>
 #include <vector>
 #include <numeric>
 #include <math.h>       /* floor */
@@ -9,7 +10,54 @@ using namespace std;
 
 int compute_fuel(int module_mass)
 {
-    return int(floor(float(module_mass)/3.0)) - 2 ;
+    // Fuel cannot be negative
+    return std::max(0, static_cast<int>(floor(module_mass/3.0)) - 2) ;
+}
+
+// Solve puzzle #1
+template <typename T>
+constexpr int solve_puzzle1(T data)
+{
+    // Puzzle #1
+    auto accum_fuel_module_only = [](int sum, int val)
+    {
+        return std::move(sum) + compute_fuel(val);
+    };
+
+    int tot_fuel1 = std::accumulate(
+        data.begin(),
+        data.end(),
+        0,
+        accum_fuel_module_only
+    );
+
+    return tot_fuel1;
+}
+
+// Solve puzzle #2
+template <typename T>
+constexpr int solve_puzzle2(T data)
+{
+    auto accum_fuel_module_and_fuel = [](int sum, int val)
+    {
+        int this_module_fuel = val;
+        do
+        {
+            this_module_fuel = compute_fuel(this_module_fuel);
+            sum += this_module_fuel;
+        }
+        while (this_module_fuel > 0);
+
+        return std::move(sum);
+    };
+
+    int tot_fuel2 = std::accumulate(
+        data.begin(),
+        data.end(),
+        0,
+        accum_fuel_module_and_fuel);
+
+    return tot_fuel2;
 }
 
 int main(int argc, char *argv[])
@@ -32,39 +80,22 @@ int main(int argc, char *argv[])
     // Reading the data
     auto data = myutils::read_file<int, std::vector<int> >(filename);
 
-    // Puzzle #1
-    auto accum_fuel_module_only = [](int sum, int val)
-    {
-        return std::move(sum) + compute_fuel(val);
-    };
+    // --------- Puzzle #1 ---------
+    // Verify examples
+    assert(solve_puzzle1<vector<int>>({12})     == 2     && "Error verifying puzzle #1");
+    assert(solve_puzzle1<vector<int>>({14})     == 2     && "Error verifying puzzle #1");
+    assert(solve_puzzle1<vector<int>>({1969})   == 654   && "Error verifying puzzle #1");
+    assert(solve_puzzle1<vector<int>>({100756}) == 33583 && "Error verifying puzzle #1");
 
-    int tot_fuel1 = std::accumulate(
-        data.begin(),
-        data.end(),
-        0,
-        accum_fuel_module_only
-    );
+    // Solve puzzle #1
+    std::cout << "Answer for puzzle #1: "<< solve_puzzle1(data) << std::endl;
 
-    std::cout << "Answer puzzle #1: "<< tot_fuel1 << std::endl;
+    // --------- Puzzle #2 ---------
+    // Verify examples
+    assert(solve_puzzle2<vector<int>>({14})     == 2     && "Error verifying puzzle #2");
+    assert(solve_puzzle2<vector<int>>({1969})   == 966   && "Error verifying puzzle #2");
+    assert(solve_puzzle2<vector<int>>({100756}) == 50346 && "Error verifying puzzle #2");
 
-    // Puzzle #2
-    auto accum_fuel_module_and_fuel = [](int sum, int val)
-    {
-        auto this_module_fuel = compute_fuel(val);
-
-        while (this_module_fuel > 0)
-        {
-            sum += this_module_fuel;
-            this_module_fuel = compute_fuel(this_module_fuel);
-        }
-        return std::move(sum);
-    };
-
-    int tot_fuel2 = std::accumulate(
-        data.begin(),
-        data.end(),
-        0,
-        accum_fuel_module_and_fuel);
-
-    std::cout << "Answer puzzle #2: "<< tot_fuel2 << std::endl;
+    // Solve puzzle #2
+    std::cout << "Answer for puzzle #2: "<< solve_puzzle2(data) << std::endl;
 }

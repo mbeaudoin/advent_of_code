eff73a24939d280b1a397ad5155cc933bb4304de
Martin Beaudoin <beaudoin.martin@gmail.com>
2019-11-30 22:10:23 -0500

    Solution for day #2

--- 2018/day_02/puzzle.cpp
+++ 2018/day_02/puzzle.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
+#include <string>
 #include <vector>
 #include <set>
+#include <map>
+#include <algorithm>
+
 #include "include/myutils.h"
 
 using namespace std;
+
+// Function for puzzle #2
+std::string find_differences(std::string s1, std::string s2)
+{
+  std::string retValue = "";
+
+  for (int k=0; k< s1.size(); k++)
+  {
+    if (s1[k] == s2[k])
+      retValue.append(1, s1[k]);
+  }
+  return retValue;
+
+}
 
 int main(int argc, char *argv[])
 {
@@ -23,12 +41,61 @@ int main(int argc, char *argv[])
     }
 
     // Reading the data
-    auto data = myutils::read_file<int, std::vector<int> >(filename);
+    std::vector<std::string> ids;
+
+    ids = myutils::read_file<string, std::vector<string> >(filename);
 
     // Puzzle #1
-    auto answer = 42;
-    std::cout << "Answer puzzle #1: "<< answer << std::endl;
+    int nbrs2 = 0;
+    int nbrs3 = 0;
+    for (auto& idd : ids)
+    {
+        std::map<char, int> char_counter;
+        std::set<int>       multiples;
+
+        // count occurences of characters
+        for (auto& c : idd)
+        {
+            char_counter[c] = char_counter[c] + 1;
+        }
+
+        // Use a set to find individual totals
+        for(auto&p : char_counter)
+        {
+            multiples.insert(p.second);
+            // std::cout << "Key: " << p.first << " : Value: " << p.second << std::endl;
+        }
+
+        // Find 2s and 3s
+        for(auto&p : multiples)
+        {
+            // std::cout << "multiples: " << p << std::endl;
+
+            if (p == 2)
+                nbrs2++;
+            else if (p == 3)
+                nbrs3++;
+        }
+    }
+    int checksum = nbrs2 * nbrs3;
+    // Report
+    // std::cout << "Nbr of 2s: " << nbrs2 << std::endl;
+    // std::cout << "Nbr of 3s: " << nbrs3 << std::endl;
+
+    std::cout << "Answer puzzle #1: " << checksum << std::endl;
 
     // Puzzle #2
-    std::cout << "Answer puzzle #2: "<< answer << std::endl;
+    for (int i=0; i<ids.size(); i++)
+    {
+        for (int j=i; j<ids.size(); j++)
+        {
+            std::string common_chars = find_differences(ids[i], ids[j]);
+
+            if(common_chars.size() == ids[i].size() - 1)
+            {
+                std::cout << "Answer puzzle #2: " << common_chars << std::endl;
+            }
+        }
+    }
+
 }

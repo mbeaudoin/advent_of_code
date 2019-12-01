4538f5442167a73db48168fc953e25ccd0c6c8e9
Martin Beaudoin <beaudoin.martin@gmail.com>
2019-11-30 22:45:29 -0500

    Solution for day #3

--- 2018/day_03/puzzle.cpp
+++ 2018/day_03/puzzle.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
 #include <vector>
 #include <set>
+#include <string>
+#include <set>
+#include <map>
+#include <fstream>
+#include <algorithm>
+#include <sstream>
 #include "include/myutils.h"
+
+using namespace std;
+
+struct patch {
+    int number;
+    int offset_x;
+    int offset_y;
+    int size_x;
+    int size_y;
+
+    friend std::istream & operator >>(std::istream & in, patch & p)
+    {
+        char c;
+        in >> c >> p.number >> c >> p.offset_x >> c >> p.offset_y >> c >> p.size_x >> c >> p.size_y ;
+        return in;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const patch& p)
+    {
+        return os << "entry: " << p.number
+            << " : offset_x: " << p.offset_x
+            << " offset_y: " << p.offset_y
+            << " size_x: " << p.size_x
+            << " size_y: " << p.size_y;
+    }
+};
 
 using namespace std;
 
@@ -23,12 +55,84 @@ int main(int argc, char *argv[])
     }
 
     // Reading the data
-    auto data = myutils::read_file<int, std::vector<int> >(filename);
+    std::vector<patch> listPatch = myutils::read_file<patch, std::vector<patch> >(filename);
 
     // Puzzle #1
-    auto answer = 42;
-    std::cout << "Answer puzzle #1: "<< answer << std::endl;
+    // Puzzle #1
+    int maxSizeX = 0;
+    int maxSizeY = 0;
+
+    for (auto& p : listPatch)
+    {
+        int sizeX = p.offset_x + p.size_x;
+        int sizeY = p.offset_y + p.size_y;
+
+        if(sizeX > maxSizeX)
+            maxSizeX = sizeX;
+
+        if(sizeY > maxSizeY)
+            maxSizeY = sizeY;
+    }
+
+    // std::cout << "maxSizeX: "<< maxSizeX << std::endl;
+    // std::cout << "maxSizeY: "<< maxSizeY << std::endl;
+
+    int cloth[maxSizeX][maxSizeY];
+    // std::cout << "Size of cloth: " << sizeof(cloth) << std::endl;
+
+    std::fill( &cloth[0][0], &cloth[0][0] + maxSizeX*maxSizeY, 0 );
+
+    // Fill cloth with patches
+    for (auto& p : listPatch)
+    {
+        for (int x=p.offset_x; x<p.offset_x + p.size_x; x++)
+        {
+            for (int y=p.offset_y; y<p.offset_y + p.size_y; y++)
+            {
+                cloth[x][y] += 1;
+            }
+        }
+
+    }
+
+    // Compute common area
+    int commonArea = 0;
+    for(int x=0; x< maxSizeX; x++)
+    {
+        for (int y=0; y< maxSizeY; y++)
+        {
+            if (cloth[x][y] > 1)
+            {
+                commonArea++;
+            }
+        }
+    }
+
+    std::cout << "Answer puzzle #1: " << commonArea << std::endl;
 
     // Puzzle #2
-    std::cout << "Answer puzzle #2: "<< answer << std::endl;
+    // intact claim
+
+    for (auto& p : listPatch)
+    {
+        bool overLap = false;
+
+        for (int x=p.offset_x; x<p.offset_x + p.size_x; x++)
+        {
+            for (int y=p.offset_y; y<p.offset_y + p.size_y; y++)
+            {
+                if(cloth[x][y] != 1)
+                {
+                    overLap = true;
+                }
+            }
+        }
+
+        if(!overLap)
+        {
+            std::cout << "Answer puzzle #2: " << p.number << std::endl;
+        }
+
+    }
+
 }

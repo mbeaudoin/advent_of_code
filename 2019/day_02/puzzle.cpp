4cfac0cfd1b74501b780b47eb07e0eeb8f18da38
Martin Beaudoin <beaudoin.martin@gmail.com>
2019-12-02 02:25:09 -0500

    Solution for day #2

--- 2019/day_02/puzzle.cpp
+++ 2019/day_02/puzzle.cpp
@@ -15,14 +15,61 @@ using namespace std;
 template <typename T>
 constexpr int solve_puzzle1(T data)
 {
-    return 42;
+    int currentIndex = 0;
+    int opCode = data[currentIndex++];
+
+    while(opCode != 99)
+    {
+        int indexParam1 = data[currentIndex++];
+        int indexParam2 = data[currentIndex++];
+        int indexParam3 = data[currentIndex++];
+
+        if(opCode == 1)  // 1: Addition
+        {
+            data[indexParam3] = data[indexParam1] + data[indexParam2];
+        }
+        else             // 2: multiplication
+        {
+            data[indexParam3] = data[indexParam1] * data[indexParam2];
+        }
+
+        opCode = data[currentIndex++];
+    }
+
+    return data[0];
 }
 
 // Solve puzzle #2
 template <typename T>
-constexpr int solve_puzzle2(T data)
+constexpr int solve_puzzle2(T data, int targetOutput)
 {
-    return 42;
+    int indexNoun = 1;
+    int indexVerb = 2;
+    int retValue = -1;
+    bool found = false;
+
+    // Make a copy
+    T origData = data;
+
+    for(int noun=0; noun <100 && !found; noun++)
+    {
+        for(int verb=0; verb <100 && !found; verb++)
+        {
+            // Restore data
+            data = origData;
+
+            data[indexNoun] = noun;
+            data[indexVerb] = verb;
+
+            if(solve_puzzle1(data) == targetOutput)
+            {
+                found = true;
+                retValue = 100*noun + verb;
+            }
+        }
+    }
+
+    return retValue;
 }
 
 int main(int argc, char *argv[])
@@ -43,21 +90,25 @@ int main(int argc, char *argv[])
     }
 
     // Reading the data
-    auto data = myutils::read_file<int, std::vector<int> >(filename);
+    auto data = myutils::read_file_csv<int, std::vector<int> >(filename);
 
     // --------- Puzzle #1 ---------
     // Verify puzzle1 examples
-    const auto example1 = 1;
-    assert(solve_puzzle1<vector<int>>({example1}) == 42 && "Error verifying puzzle #1");
+    assert(solve_puzzle1<vector<int>>({1,9,10,3,2,3,11,0,99,30,40,50}) == 3500 && "Error verifying puzzle #1");
+    assert(solve_puzzle1<vector<int>>({1,0,0,0,99})                    == 2    && "Error verifying puzzle #1");
+    assert(solve_puzzle1<vector<int>>({2,3,0,3,99})                    == 2    && "Error verifying puzzle #1");
+    assert(solve_puzzle1<vector<int>>({2,4,4,5,99,0})                  == 2    && "Error verifying puzzle #1");
+    assert(solve_puzzle1<vector<int>>({1,1,1,4,99,5,6,0,99})           == 30   && "Error verifying puzzle #1");
 
     // Solve puzzle #1
+    data[1] = 12;
+    data[2] = 2;
     std::cout << "Answer for puzzle #1: "<< solve_puzzle1(data) << std::endl;
 
     // --------- Puzzle #2 ---------
     // Verify puzzle2 examples
-    const auto example2 = 2;
-    assert(solve_puzzle2<vector<int>>({example2}) == 42 && "Error verifying puzzle #2");
+    assert(solve_puzzle2(data, 4576384) == 1202 && "Error verifying puzzle #2");
 
     // Solve puzzle #2
-    std::cout << "Answer for puzzle #2: "<< solve_puzzle2(data) << std::endl;
+    std::cout << "Answer for puzzle #2: "<< solve_puzzle2(data, 19690720) << std::endl;
 }
--- 2019/include/myutils.h
+++ 2019/include/myutils.h
@@ -38,10 +38,36 @@ template <typename T_value = char,
     }
 
 
-    inline bool file_exists (const std::string& name)
+template <typename T_value = char,
+    typename T_container = std::vector<T_value>>
+    auto read_file_csv(const std::string filename, const bool debug=false, char sepValue=',')
+    {
+        T_container buffer;
+
+        std::ifstream file(filename);
+
+        std::string number_as_string;
+
+        while (std::getline(file, number_as_string, sepValue))
+        {
+            buffer.push_back(std::stoi(number_as_string));
+        }
+
+        if(debug)
+        {
+            std::cout << "Buffer len: " << buffer.size() << std::endl;
+
+            for (auto& val : buffer)
+                std::cout << val << std::endl;
+        }
+
+        return buffer;
+    }
+
+inline bool file_exists (const std::string& name)
     {
         return ( access( name.c_str(), F_OK ) != -1 );
     }
-}
 
+}
 #endif  // MYUTILS_H

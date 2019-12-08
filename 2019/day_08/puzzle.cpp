3315f4e6e1f578d06595cd506152b73d6f4c5e21
Martin Beaudoin <beaudoin.martin@gmail.com>
2019-12-08 08:59:39 -0500

    Solution for day #8

--- 2019/day_08/puzzle.cpp
+++ 2019/day_08/puzzle.cpp
@@ -6,23 +6,141 @@
 
 #include <iostream>
 #include <cassert>
+#include <array>
 #include <vector>
 #include "myutils.h"
 
 using namespace std;
 
+const int imageWidth = 25;
+const int imageHeight = 6;
+const int nbpPixPerLayer = imageWidth * imageHeight;
+
+typedef array<int, nbpPixPerLayer> imageLayer;
+
+template <typename T>
+void
+extractImageLayers(T data, vector<imageLayer>& image)
+{
+    int nbrLayers = data[0].size() / (imageWidth*imageHeight);
+
+    image.resize(nbrLayers);
+    int nLayer = 0;
+    for(int k=0; k<data[0].size();)
+    {
+        for(int i = 0; i<nbpPixPerLayer; i++)
+        {
+            // Convert ASCII to integer
+            image[nLayer][i] = data[0][k++] - '0';
+        }
+        nLayer++;
+    }
+
+#if 0
+    cout << "nbrLayers: " << nbrLayers << endl;
+
+    for(auto layer : image)
+    {
+        for(auto p : layer)
+        {
+            cout << p;
+        }
+        cout << endl << endl;
+    }
+#endif
+    return;
+}
+
+vector<int>
+calcNbrPixels(vector<imageLayer> image, int pixValue)
+{
+    vector<int> pixNbrPerLayer;
+
+    for(auto layer : image)
+    {
+        int sum = 0;
+        for(auto p : layer)
+        {
+            if(p == pixValue)
+                sum++;
+        }
+        pixNbrPerLayer.push_back(sum);
+    }
+
+#if 0
+    cout << "nbr pixvalue: " << pixValue << endl;
+    for(auto n : pixNbrPerLayer)
+        cout << n << " : " ;
+    cout << endl;
+#endif
+
+    return pixNbrPerLayer;
+}
+
 // Solve puzzle #1
 template <typename T>
-constexpr int solve_puzzle1(T data)
+int solve_puzzle1(T data)
 {
-    return 42;
+    // Extract layers
+    vector<imageLayer> image;
+    extractImageLayers(data, image);
+
+    vector<int> nbrZeros = calcNbrPixels(image, 0);
+    vector<int> nbrOnes = calcNbrPixels(image, 1);
+    vector<int> nbrTwos = calcNbrPixels(image, 2);
+
+    // Find layer with minimal number of zeros
+    int indexMinZero = distance(nbrZeros.begin(), std::min_element(nbrZeros.begin(), nbrZeros.end()));
+
+    // Return answer
+    return nbrOnes[indexMinZero] * nbrTwos[indexMinZero];
 }
 
 // Solve puzzle #2
 template <typename T>
-constexpr int solve_puzzle2(T data)
+string solve_puzzle2(T data)
 {
-    return 42;
+    // Extract layers
+    vector<imageLayer> image;
+    extractImageLayers(data, image);
+
+    const int transparentPixel = 2;
+
+    // Use reverse iterator for handling layers from bottom to top (backward)
+    vector<imageLayer>::reverse_iterator rit = image.rbegin();
+
+    imageLayer renderedImage = *rit;
+
+#if 0
+    for(auto p : renderedImage)
+        cout << p << ":";
+    cout << endl;
+#endif
+
+    // Backward stepping through all layers, handling transparent pixels
+    for (; rit!= image.rend(); ++rit)
+    {
+        for(int i=0; i<nbpPixPerLayer; i++)
+        {
+            int pixel = (*rit)[i];
+            if(pixel != transparentPixel)
+            {
+                renderedImage[i] = pixel;
+            }
+        }
+    }
+
+    // Render image using dots and spaces
+    cout << endl;
+    int k = 0;
+    for(int i=0; i<nbpPixPerLayer; i++)
+    {
+        cout << (renderedImage[k++] == 1 ? "."  : " ");
+        cout << ((i+1) % imageWidth == 0 ? "\n" : "");
+    }
+    cout << endl;
+
+    return "KFABY";
 }
 
 int main(int argc, char *argv[])
@@ -43,20 +161,18 @@ int main(int argc, char *argv[])
     }
 
     // Reading the data
-    auto data = myutils::read_file<int, std::vector<int> >(filename);
+    auto data = myutils::read_file<string, std::vector<string> >(filename);
 
     // --------- Puzzle #1 ---------
     // Verify puzzle1 examples
-    const auto example1 = 1;
-    assert(solve_puzzle1<vector<int>>({example1}) == 42 && "Error verifying puzzle #1");
+    assert(solve_puzzle1(data) == 1716 && "Error verifying puzzle #1");
 
     // Solve puzzle #1
     std::cout << "Answer for puzzle #1: "<< solve_puzzle1(data) << std::endl;
 
     // --------- Puzzle #2 ---------
     // Verify puzzle2 examples
-    const auto example2 = 2;
-    assert(solve_puzzle2<vector<int>>({example2}) == 42 && "Error verifying puzzle #2");
+    assert(solve_puzzle2(data) == "KFABY" && "Error verifying puzzle #2");
 
     // Solve puzzle #2
     std::cout << "Answer for puzzle #2: "<< solve_puzzle2(data) << std::endl;

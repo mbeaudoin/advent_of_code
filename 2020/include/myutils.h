eda39b41dab382d77622c0c7c7436e07177fde05
Martin Beaudoin <beaudoin.martin@gmail.com>
2020-11-28 21:04:48 -0500

    AOC 2020: initial commit and templates

---
+++ 2020/include/myutils.h
@@ -0,0 +1,150 @@
+//
+// Copyright (C) Martin Beaudoin. 2020. All Rights Reserved.
+//
+// See the repository's LICENSE file for the full license details.
+//
+
+#ifndef MYUTILS_H
+#define MYUTILS_H
+
+#include <fstream>
+#include <streambuf>
+#include <iterator>
+#include <vector>
+#include <algorithm>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <math.h>
+
+namespace myutils
+{
+
+template <typename T_value = char,
+          typename T_container = std::vector<T_value>>
+    auto read_file(const std::string filename, const bool debug=false)
+    {
+        T_container buffer;
+
+        std::ifstream file(filename);
+
+        std::copy(
+            std::istream_iterator<T_value>(file),
+            std::istream_iterator<T_value>(),
+            std::back_inserter(buffer)
+        );
+
+        if(debug)
+        {
+            std::cout << "Buffer len: " << buffer.size() << std::endl;
+
+            for (auto& val : buffer)
+                std::cout << val << std::endl;
+        }
+
+        return buffer;
+    }
+
+
+template <typename T_value = char,
+          typename T_container = std::vector<T_value>>
+    auto read_file_csv(const std::string filename, const bool debug=false, char sepValue=',')
+    {
+        T_container buffer;
+
+        std::ifstream file(filename);
+
+        std::string number_as_string;
+
+        while (std::getline(file, number_as_string, sepValue))
+        {
+            buffer.push_back(std::stoll(number_as_string));
+        }
+
+        if(debug)
+        {
+            std::cout << "Buffer len: " << buffer.size() << std::endl;
+
+            for (auto& val : buffer)
+                std::cout << val << std::endl;
+        }
+
+        return buffer;
+    }
+
+    inline bool file_exists (const std::string& name)
+    {
+        return ( access( name.c_str(), F_OK ) != -1 );
+    }
+
+
+    // 2D point class
+    class point
+    {
+    public:
+        int x;
+        int y;
+
+        point() : x(0), y(0) {}
+        point(int valx, int valy) : x(valx), y(valy) {}
+        point(const point& p) : x(p.x), y(p.y) {}
+        point (point&& p): x(std::move(p.x)), y(std::move(p.y)) {}
+
+        point& operator=(point other)
+        {
+            x = other.x;
+            y = other.y;
+            return *this;
+        }
+
+        point operator+(point other)
+        {
+            other.x += x;
+            other.y += y;
+
+            return other;
+        }
+
+        // Distance squared from origin
+        int dist2() const
+        {
+            return x*x + y*y;
+        }
+
+        // compare for order.
+        bool operator <(const point& pt) const
+        {
+            return pt.dist2() > dist2();
+        }
+
+    };
+
+    bool operator==(const point &p1, const point& p2)
+    {
+        return p1.x == p2.x && p1.y == p2.y;
+    }
+
+    bool operator!=(const point &p1, const point& p2)
+    {
+        return p1.x != p2.x || p1.y != p2.y;
+    }
+
+    double atan2InDeg(const point &p)
+    {
+        double deg = atan2(p.y, p.x) * 180.0 / M_PI;
+
+        return deg;
+    }
+
+    std::ostream& operator<<(std::ostream& os, const point& p)
+    {
+        return os << p.x
+            << " "
+            << p.y;
+    }
+
+    template <typename T> int sgn(T val)
+    {
+        return (T(0) < val) - (val < T(0));
+    }
+}
+#endif  // MYUTILS_H
---
+++ 2020/template/template_main.cpp
@@ -0,0 +1,63 @@
+//
+// Copyright (C) Martin Beaudoin. 2020. All Rights Reserved.
+//
+// See the repository's LICENSE file for the full license details.
+//
+
+#include <iostream>
+#include <cassert>
+#include <vector>
+#include "myutils.h"
+
+using namespace std;
+
+// Solve puzzle #1
+template <typename T>
+constexpr int solve_puzzle1(T data)
+{
+    return 42;
+}
+
+// Solve puzzle #2
+template <typename T>
+constexpr int solve_puzzle2(T data)
+{
+    return 42;
+}
+
+int main(int argc, char *argv[])
+{
+    // Need a command-line parameter for the input filename
+    if (argc != 2) {
+        cerr << "Error: missing input" << endl;
+        return EXIT_FAILURE;
+    }
+
+    string filename(argv[1]);
+
+    // This file needs to exist
+    if (! myutils::file_exists(filename))
+    {
+        cerr << "Error: nonexistent file: " << filename << endl;
+        return EXIT_FAILURE;
+    }
+
+    // Reading the data
+    auto data = myutils::read_file<int, std::vector<int> >(filename);
+
+    // --------- Puzzle #1 ---------
+    // Verify puzzle1 examples
+    const auto example1 = 1;
+    assert(solve_puzzle1<vector<int>>({example1}) == 42 && "Error verifying puzzle #1");
+
+    // Solve puzzle #1
+    std::cout << "Answer for puzzle #1: "<< solve_puzzle1(data) << std::endl;
+
+    // --------- Puzzle #2 ---------
+    // Verify puzzle2 examples
+    const auto example2 = 2;
+    assert(solve_puzzle2<vector<int>>({example2}) == 42 && "Error verifying puzzle #2");
+
+    // Solve puzzle #2
+    std::cout << "Answer for puzzle #2: "<< solve_puzzle2(data) << std::endl;
+}

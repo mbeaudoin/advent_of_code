78e19c1c46b4c07c039a2d99177feb339933ce08
Martin Beaudoin <4019759+mbeaudoin@users.noreply.github.com>
2023-12-01 07:56:51 -0500

    AOC 2023: Solution for day 1

--- 2023/day_01/puzzle.cpp
+++ 2023/day_01/puzzle.cpp
@@ -9,6 +9,7 @@
 #include <list>
 #include <numeric>
 #include <algorithm>
+#include <regex>
 #include "myutils.h"
 
 using namespace std;
@@ -24,30 +25,35 @@ long solve_puzzle1(T data)
     }
 #endif
 
-    std::list<long> calories;
-    long sumCalories = 0L;
+    long sumCalibrationNumbers = 0L;
 
-    for(auto vi : data )
+    auto toDigit = [](auto c)
+        {
+            return c - '0';
+        };
+
+    auto isDigit = [](auto c)
+        {
+            return isdigit(c);
+        };
+
+    for(auto s : data )
     {
-        if(vi.empty())
+        std::string onlyDigits;
+
+        std::copy_if(s.begin(), s.end(), std::back_inserter(onlyDigits), isDigit);
+
+        if(onlyDigits.size() > 0)
         {
-            calories.push_back(sumCalories);
-            sumCalories = 0L;
-        }
-        else
-        {
-            long val = std::stol(vi);
-            sumCalories += val;
+            int calibCode = 10*toDigit(onlyDigits[0]) + toDigit(onlyDigits[onlyDigits.size()-1]);
+
+            // std::cout << "Code: "<< s << " : digits: " << onlyDigits << ": " << calibCode << std::endl;
+
+            sumCalibrationNumbers += calibCode;
         }
     }
-    // Add last block
-    calories.push_back(sumCalories);
-
-    // Descending sort
-    calories.sort(std::greater<>());
-
-    // Return max value
-    return *calories.begin();
+    // Return sum
+    return sumCalibrationNumbers;
 }
 
 // Solve puzzle #2
@@ -61,35 +67,61 @@ long solve_puzzle2(T data)
     }
 #endif
 
-    std::vector<long> calories;
-    long sumCalories = 0L;
+    long sumCalibrationNumbers = 0L;
 
-    for(auto vi : data )
+    auto toDigit = [](auto c)
+        {
+            return c - '0';
+        };
+
+    auto isDigit = [](auto c)
+        {
+            return isdigit(c);
+        };
+
+    for(auto s : data )
     {
-        if(vi.empty())
+        std::string onlyDigits;
+
+        //std::cout << "Original string: " << s << "  --> ";
+
+        // First, replace string instances (combo)
+        s = std::regex_replace(s, std::regex("zerone"),    "01");
+        s = std::regex_replace(s, std::regex("twone"),     "21");
+        s = std::regex_replace(s, std::regex("sevenine"),  "79");
+        s = std::regex_replace(s, std::regex("eightwo"),   "82");
+        s = std::regex_replace(s, std::regex("eighthree"), "83");
+        s = std::regex_replace(s, std::regex("threeight"), "38");
+        s = std::regex_replace(s, std::regex("fiveight"),  "58");
+        s = std::regex_replace(s, std::regex("oneight"),   "18");
+
+        // Next standalone instances
+        s = std::regex_replace(s, std::regex("zero"),  "0");
+        s = std::regex_replace(s, std::regex("one"),   "1");
+        s = std::regex_replace(s, std::regex("two"),   "2");
+        s = std::regex_replace(s, std::regex("three"), "3");
+        s = std::regex_replace(s, std::regex("four"),  "4");
+        s = std::regex_replace(s, std::regex("five"),  "5");
+        s = std::regex_replace(s, std::regex("six"),   "6");
+        s = std::regex_replace(s, std::regex("seven"), "7");
+        s = std::regex_replace(s, std::regex("eight"), "8");
+        s = std::regex_replace(s, std::regex("nine"),  "9");
+
+        // std::cout << "cleaned string: " << s << std::endl;
+
+        std::copy_if(s.begin(), s.end(), std::back_inserter(onlyDigits), isDigit);
+
+        if(onlyDigits.size() > 0)
         {
-            calories.push_back(sumCalories);
-            sumCalories = 0L;
-        }
-        else
-        {
-            long val = std::stol(vi);
-            sumCalories += val;
+            int calibCode = 10*toDigit(onlyDigits[0]) + toDigit(onlyDigits[onlyDigits.size()-1]);
+
+            // std::cout << "Code: "<< s << " : digits: " << onlyDigits << ": " << calibCode << std::endl;
+
+            sumCalibrationNumbers += calibCode;
         }
     }
-
-    // Add last block
-    calories.push_back(sumCalories);
-
-    // Descending sort
-    std::sort(calories.begin(),calories.end(), greater<int>());
-
-    // Keep first 3 entries
-    calories.resize(3);
-
     // Return sum
-    return std::accumulate(calories.begin(), calories.end(), 0L);
-
+    return sumCalibrationNumbers;
 }
 
 int main(int argc, char *argv[])
@@ -111,37 +143,37 @@ int main(int argc, char *argv[])
 
     // Reading the data
     std::vector<string> data;
-    myutils::read_file(data, filename, true, true);
+    myutils::read_file(data, filename, true, true, false);
 
     const vector<string> example = {
-        "1000",
-        "2000",
-        "3000",
-        "",
-        "4000",
-        "",
-        "5000",
-        "6000",
-        "",
-        "7000",
-        "8000",
-        "9000",
-        "",
-        "10000",
+        "1abc2",
+        "pqr3stu8vwx",
+        "a1b2c3d4e5f",
+        "treb7uchet"
     };
 
     // --------- Puzzle #1 ---------
     // Verify puzzle1 examples
-    assert(solve_puzzle1<vector<string>>(example) == 24000 && "Error verifying puzzle #1");
+    assert(solve_puzzle1<vector<string>>(example) == 142 && "Error verifying puzzle #1");
 
     // Solve puzzle #1
-    std::cout << "Answer for puzzle #1: "<< solve_puzzle1(data) << std::endl;
+    std::cout << "Answer for puzzle #1: "<< solve_puzzle1(data) << std::endl;  // 54644
+
+    const vector<string> example2 = {
+        "two1nine",
+        "eightwothree",
+        "abcone2threexyz",
+        "xtwone3four",
+        "4nineeightseven2",
+        "zoneight234",
+        "7pqrstsixteen"
+    };
 
     // --------- Puzzle #2 ---------
     // Verify puzzle2 examples
-    assert(solve_puzzle2<vector<string>>(example) == 45000 && "Error verifying puzzle #2");
+    assert(solve_puzzle2<vector<string>>(example2) == 281 && "Error verifying puzzle #2");
 
     // Solve puzzle #2
-    std::cout << "Answer for puzzle #2: "<< solve_puzzle2(data) << std::endl;
+    std::cout << "Answer for puzzle #2: "<< solve_puzzle2(data) << std::endl;  // 53348
 
 }
---
+++ 2023/include/myutils.h
@@ -0,0 +1,201 @@
+//
+// Copyright (C) Martin Beaudoin. 2020. All Rights Reserved.
+//
+// See the repository's LICENSE file for the full license details.
+//
+
+#ifndef MYUTILS_H
+#define MYUTILS_H
+
+#include <fstream>
+#include <streambuf>
+#include <iterator>
+#include <vector>
+#include <algorithm>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <math.h>
+
+namespace myutils
+{
+
+
+template <typename T_value = char,
+          typename T_container = std::vector<T_value>>
+int
+read_file(
+    T_container& buffer,
+    const std::string filename,
+    const bool keepSpaces=false,
+    const bool keepEmptyLines=false,
+    const bool debug=false
+)
+{
+    std::ifstream file(filename);
+
+    std::copy(
+        std::istream_iterator<T_value>(file),
+        std::istream_iterator<T_value>(),
+        std::back_inserter(buffer)
+    );
+
+    if(debug)
+    {
+        std::cout << "Buffer len: " << buffer.size() << std::endl;
+
+        for (auto& val : buffer)
+            std::cout << val << std::endl;
+    }
+
+    return 0;
+}
+
+
+// template <typename T_value = char,
+//          typename T_container = std::vector<T_value>>
+template <typename T_container = std::vector<int>>
+    auto read_file_csv(const std::string filename, const bool debug=false, char sepValue=',')
+    {
+        T_container buffer;
+
+        std::ifstream file(filename);
+
+        std::string number_as_string;
+
+        while (std::getline(file, number_as_string, sepValue))
+        {
+            buffer.push_back(std::stoll(number_as_string));
+        }
+
+        if(debug)
+        {
+            std::cout << "Buffer len: " << buffer.size() << std::endl;
+
+            for (auto& val : buffer)
+                std::cout << val << std::endl;
+        }
+
+        return buffer;
+    }
+
+    inline bool file_exists (const std::string& name)
+    {
+        return ( access( name.c_str(), F_OK ) != -1 );
+    }
+
+
+    // 2D point class
+    class point
+    {
+    public:
+        int x;
+        int y;
+
+        point() : x(0), y(0) {}
+        point(int valx, int valy) : x(valx), y(valy) {}
+        point(const point& p) : x(p.x), y(p.y) {}
+        point (point&& p): x(std::move(p.x)), y(std::move(p.y)) {}
+
+        point& operator=(point other)
+        {
+            x = other.x;
+            y = other.y;
+            return *this;
+        }
+
+        point operator+(point other)
+        {
+            other.x += x;
+            other.y += y;
+
+            return other;
+        }
+
+        // Distance squared from origin
+        int dist2() const
+        {
+            return x*x + y*y;
+        }
+
+        // compare for order.
+        bool operator <(const point& pt) const
+        {
+            return pt.dist2() > dist2();
+        }
+
+    };
+
+    bool operator==(const point &p1, const point& p2)
+    {
+        return p1.x == p2.x && p1.y == p2.y;
+    }
+
+    bool operator!=(const point &p1, const point& p2)
+    {
+        return p1.x != p2.x || p1.y != p2.y;
+    }
+
+    double atan2InDeg(const point &p)
+    {
+        double deg = atan2(p.y, p.x) * 180.0 / M_PI;
+
+        return deg;
+    }
+
+    std::ostream& operator<<(std::ostream& os, const point& p)
+    {
+        return os << p.x
+            << " "
+            << p.y;
+    }
+
+    template <typename T> int sgn(T val)
+    {
+        return (T(0) < val) - (val < T(0));
+    }
+}
+
+// Specialized template
+template <>
+int
+myutils::read_file(
+    std::vector<std::string>& buffer,
+    const std::string filename,
+    const bool keepSpaces,
+    const bool keepEmptyLines,
+    const bool debug
+)
+{
+    std::ifstream file(filename);
+
+    if(keepSpaces || keepEmptyLines)
+    {
+        while(file.good())
+        {
+            std::string line;
+            getline(file, line);
+            if(line.size() > 0 || keepEmptyLines)
+                buffer.push_back(line);
+        }
+    }
+    else
+    {
+        std::copy(
+            std::istream_iterator<std::string>(file),
+            std::istream_iterator<std::string>(),
+            std::back_inserter(buffer)
+        );
+    }
+
+    if(debug)
+    {
+        std::cout << "Buffer len: " << buffer.size() << std::endl;
+
+        for (auto& val : buffer)
+            std::cout << val << std::endl;
+    }
+
+    return 0;
+}
+
+#endif  // MYUTILS_H
